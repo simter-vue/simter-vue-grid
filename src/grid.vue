eb2bb143c45537df5a8e0bd2d3f34ca377c696d2
RJ <rongjihuang@gmail.com>
2019-07-16 11:47:03 +0800

    Implement auto horizon scroll
    Support grid property `lastColumnIsAutoWidth` and default set to false

--- src/grid.vue
+++ src/grid.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="st-grid">
     <div class="header">
-      <table border="1">
+      <table
+        border="1"
+        :style="{left: v.scrollLeft + 'px', width: 'calc(100% - ' + v.scrollBarWidth + 'px)'}"
+      >
         <st-colgroup :columns="columns"></st-colgroup>
         <st-thead :columns="columns"></st-thead>
       </table>
     </div>
-    <div class="content">
+    <div class="content" @scroll="v.scrollLeft = -1 * $event.target.scrollLeft">
       <table border="1">
         <st-colgroup :columns="columns"></st-colgroup>
         <tbody>
@@ -42,7 +45,13 @@ export default {
       default() {
         return [];
       }
-    }
+    },
+    lastColumnIsAutoWidth: { type: Boolean, required: false, default: false }
+  },
+  data: function() {
+    return {
+      v: { scrollLeft: 0, scrollBarWidth: 0, timer: null, contentEl: null }
+    };
   },
   computed: {
     flattenColumns() {
@@ -50,6 +59,31 @@ export default {
     },
     subColumns() {
       return this.flattenColumns.filter(c => c.pid);
+    }
+  },
+  mounted() {
+    if (!this.lastColumnIsAutoWidth) {
+      // watch horizon scrollbar size
+      this.v.contentEl = this.$el.querySelector(".content"); // cache content el
+      this.$_watchHorizonScrollBarSize();
+    }
+  },
+  destroyed() {
+    if (!this.lastColumnIsAutoWidth) clearInterval(this.v.timer);
+  },
+  methods: {
+    $_watchHorizonScrollBarSize() {
+      let t;
+      this.v.timer = setInterval(() => {
+        t = this.v.contentEl.offsetWidth - this.v.contentEl.clientWidth;
+        if (t != this.v.scrollBarWidth) {
+          // console.log("scrollBarWidth: %s > %s", this.v.scrollBarWidth, t);
+          this.v.scrollBarWidth = t;
+        }
+      }, 100);
+    },
+    scrollContent($event) {
+      this.v.scrollLeft = -1 * $event.target.scrollLeft;
     }
   }
 };
@@ -59,15 +93,26 @@ export default {
 .st-grid {
   display: flex;
   flex-direction: column;
+  position: relative;
 }
 .st-grid > * {
   flex: none;
+  position: relative;
 }
 .st-grid > .content {
   flex: 1 1 0%;
   overflow: auto;
 }
-.st-grid > .content > table, .st-grid > .header > table {
+.st-grid > .header {
+  overflow: hidden;
+}
+.st-grid > .header > table {
+  position: relative;
+}
+.st-grid > .content > table,
+.st-grid > .header > table {
   width: 100%;
+  table-layout: fixed;
+  border-collapse: collapse;
 }
 </style>
